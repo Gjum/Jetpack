72dc86bfee21b363ff0ffde5a45ec9b4aefc6ff0
Gjum <gjum42@gmail.com>
2013-10-19 16:02:57 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char *argv[]) {
+    sf::RenderWindow window(sf::VideoMode(800, 450), "New SFML Project");
+
+    while (window.isOpen()) {
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
